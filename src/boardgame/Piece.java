a2359c382eb960019ff67f5b089f593935c770c3
LucyC-code <lucycrowe0@gmail.com>
2025-09-25 10:37:23 +0100

    Added to Piece and Position classes

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,4 +1,35 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board){
+        this.board = board;
+        position = null;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public abstract boolean [][] possibleMoves( );
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[row].length; col++) {
+                if (mat[row][col]) {
+                    return true; // Found at least one possible move
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,4 +1,51 @@
 package boardgame;
 
+/** This code represents a position on a board using row and column indices
+ * Rows and columns are typically zero based.
+ */
+
 public class Position {
+    private int row;
+    private int column;
+
+    /**
+     * Creates a new position with the given row and column
+     * @param row the row index (0 based)
+     * @param column the column index (0 based)
+     */
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    /** Updates both the row and column at once.
+     *
+     * @param row new row index
+     * @param column new column index
+     */
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
 }
